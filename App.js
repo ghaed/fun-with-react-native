1363af301cb03d4324f95941e1be6a2ab0f40673
ghaed <Sadaf@saeedehs-air.lan>
2020-12-25 14:44:32 -0800

    Adding function to button

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up app.js to start working on your app2 ')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Change Text2" onPress={() => setOutputText('Text Changed!')}/>
       <StatusBar style="auto" />
     </View>
   );
